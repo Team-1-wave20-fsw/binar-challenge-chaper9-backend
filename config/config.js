4720e0d7cb89276f14b2c0d4ad2607a77938c228
Bayu Luky <bayuluky@Bayu-MacBook-Pro.local>
2022-07-20 08:18:16 +0700

    config : add env to configure port,db

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME_DEVELOPMENT || "postgres",
+    password: process.env.DB_PASSWORD_DEVELOPMENT || null,
+    database: process.env.DB_NAME_DEVELOPMENT || "binarch9",
+    host: process.env.DB_HOST_DEVELOPMENT || "127.0.0.1",
+    dialect: process.env.DB_DIALECT_DEVELOPMENT || "postgres",
+  },
+  test: {
+    username: process.env.DB_USERNAME_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_TEST,
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.DB_USERNAME_PRODUCTION,
+    password: process.env.DB_PASSWORD_PRODUCTION,
+    database: process.env.DB_PRODUCTION,
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+};
