3e8cbe2a0b1e2462c845a4cfe21548b73c62f48b
Agus <sukarno.agus@gmail.com>
2022-07-27 15:52:11 +0700

    fixing: add access-control-allow-origin in update profile routes

--- routes/gameplayroutes.js
+++ routes/gameplayroutes.js
@@ -6,6 +6,15 @@ const restrict = require("../middlewares/restrict");
  * @Routes "/api/gameplay"
  */
 
+ router.all("/add", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,Content-Type,Authorization"
+    );
+    next();
+});
+
 router.post("/add", restrict, gameplayController.addScores);
 
 module.exports = router;
--- routes/userroutes.js
+++ routes/userroutes.js
@@ -6,6 +6,15 @@ const restrict = require("../middlewares/restrict");
  * @Routes "/api/users"
  */
 
+router.all("/:id", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,Content-Type,Authorization"
+    );
+    next();
+});
+
 router.get("/score", userController.getUserScores);
 router.get("/", userController.getUsers);
 router.get("/:id", userController.getUserDetail);
